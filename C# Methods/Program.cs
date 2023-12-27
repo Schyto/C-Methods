da105aad2d717cb35399a14cdf30b12be53ddfff
Jaxon Panter <jmanpanter@gmail.com>
2023-12-27 09:42:05 -0600

    Finished profile configuration

--- C# Methods/Program.cs
+++ C# Methods/Program.cs
@@ -18,6 +18,14 @@ namespace MethodsExercise
 
             Console.WriteLine($"{animal}'s are great! Now what is your favorite band?");
         var band = Console.ReadLine();
+
+            Console.WriteLine($"Thanks, {userName}! Here is your profile");
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
         }
     }
 }
